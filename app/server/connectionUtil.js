747d4c1363153cca7a2013e1cfa1d090087e3861
Bardapurkar Pranit <bardapra@ap.dtcs.local>
2016-12-01 12:03:25 +0530

    reverting cahnges

--- app/server/connectionUtil.js
+++ app/server/connectionUtil.js
@@ -10,9 +10,9 @@ var authSource    = 'Quants';
 var format = util.format;
 
 //mongodb://quantico:E321_QUANTICO@ds047612.mlab.com:47612/quants
-var url = format('mongodb://%s:%s@localhost:27017/Quants',user,password);
+//var url = format('mongodb://%s:%s@localhost:27017/Quants',user,password);
 
-//var url = 'mongodb://quantico:E321_QUANTICO@ds047612.mlab.com:47612/quants';
+var url = 'mongodb://quantico:E321_QUANTICO@ds047612.mlab.com:47612/quants';
 
 var _connectionString = url;
 
